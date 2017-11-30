eac85078f763f47ddf7c1c135a9fce9983a269b9
Lukáš Lalinský <lukas@oxygene.sk>
2017-11-30 19:04:45 +0100

    Use alpine to build the binary

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,16 @@
 : ${ACOUSTID_PRIV_BIND:=0.0.0.0:5000}
 : ${ACOUSTID_PRIV_DB_URL:=postgresql://localhost/acoustid_priv}
 
-args=( -bind=$ACOUSTID_PRIV_BIND -db=$ACOUSTID_PRIV_DB_URL )
+if [ "${1:0:1}" = '-' ]
+then
+	set -- acoustid-priv-api "$@"
+fi
+
+args=()
+
+if [ "$1" = 'acoustid-priv-api' ]
+then
+    args=( -bind=$ACOUSTID_PRIV_BIND -db=$ACOUSTID_PRIV_DB_URL )
+fi
 
 exec "$@" "${args[@]}"
